daad289fba593dbde98fc050b53c6f1c47bc2076
Tomas <you@tavo.org>
2020-02-11 09:56:48 +0200

    Added try catch for debug purposes

--- TomasKireilisBot/TomasKireilisBot/Dialogs/ActivatePullRequestNotificationDialog.cs
+++ TomasKireilisBot/TomasKireilisBot/Dialogs/ActivatePullRequestNotificationDialog.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
+﻿using System;
+using Microsoft.Bot.Builder.Dialogs;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Schema;
@@ -34,12 +35,21 @@ namespace TomasKireilisBot.Dialogs
                 Text = "PullRequestNotification"
             };
             await stepContext.Context.SendActivityAsync("Adding notification... please wait. It might take some time", cancellationToken: cancellationToken);
-            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=tomasbotstorage;AccountKey=I4+e7qZyKNIiFsLR1R8XLn5AQVSq6DP3gRd3eHq2H/x3n44zUtLEqB5PIYee0PBGbQpo358xA/CUHtr/RAAPaw==;EndpointSuffix=core.windows.net");
-            CloudQueueClient queueClient = cloudStorageAccount.CreateCloudQueueClient();
-            CloudQueue queue = queueClient.GetQueueReference("Botqueue");
-            await queue.CreateIfNotExistsAsync();
-            CloudQueueMessage message = new CloudQueueMessage(JsonConvert.SerializeObject(queueMessage));
-            await queue.AddMessageAsync(message);
+            try
+            {
+                CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=tomasbotstorage;AccountKey=I4+e7qZyKNIiFsLR1R8XLn5AQVSq6DP3gRd3eHq2H/x3n44zUtLEqB5PIYee0PBGbQpo358xA/CUHtr/RAAPaw==;EndpointSuffix=core.windows.net");
+                CloudQueueClient queueClient = cloudStorageAccount.CreateCloudQueueClient();
+                CloudQueue queue = queueClient.GetQueueReference("Botqueue");
+                await queue.CreateIfNotExistsAsync();
+                CloudQueueMessage message = new CloudQueueMessage(JsonConvert.SerializeObject(queueMessage));
+                await queue.AddMessageAsync(message);
+            }
+            catch (Exception e)
+            {
+                await stepContext.Context.SendActivityAsync(e.Message, cancellationToken: cancellationToken);
+                throw;
+            }
+
             //if(queue.PeekMessageAsync(,message,cancellationToken) != null)
 
             await stepContext.Context.SendActivityAsync("Notification added", cancellationToken: cancellationToken);

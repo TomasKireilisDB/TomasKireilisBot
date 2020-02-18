2dee195dcfe61462d0e2fa09087d97e8aa7325b5
Tomas <tkir@danskebank.com>
2020-02-18 08:29:57 +0200

    Updated global variables json file

--- TomasKireilisBot/TomasKireilisBot/DataModels/Variables/BitBucketGlobalVariables.cs
+++ TomasKireilisBot/TomasKireilisBot/DataModels/Variables/BitBucketGlobalVariables.cs
@@ -5,9 +5,7 @@
         public string BaseUrl { get; set; }
         public string ProjectName { get; set; }
         public string RepositoryName { get; set; }
-        public string FullName { get; set; }
         public string UserName { get; set; }
-
         public string Password { get; set; }
         public string PersonalAccessToken { get; set; }
     }
